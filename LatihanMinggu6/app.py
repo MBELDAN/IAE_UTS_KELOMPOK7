566d397e6d62dea6adfb700083669b61000cf770
mysfarkan <116157406+mysfarkan@users.noreply.github.com>
2024-04-29 19:50:10 +0700

    Add files via upload

---
+++ LatihanMinggu6/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/premierleague')
+def premier():
+    url = "https://premier-league-standings1.p.rapidapi.com/"
+
+    headers = {
+        "X-RapidAPI-Key": "f3ca8e13f4msh46de789c7f6234cp19f80ajsn1163e1927d50",
+        "X-RapidAPI-Host": "premier-league-standings1.p.rapidapi.com"
+    }
+
+
+
+
+    response = requests.get(url, headers=headers)
+    arkan = response.json()
+    return render_template('premier.html', data=arkan)
+
+@app.route('/playerinf')
+def playerinfo():
+    url = "https://premier-league-players1.p.rapidapi.com/players"
+
+    headers = {
+        "X-RapidAPI-Key": "f5bb208837msh45f400f3016e4f8p169667jsn3fac8033682a",
+        "X-RapidAPI-Host": "premier-league-players1.p.rapidapi.com"
+    }
+
+
+    response = requests.get(url, headers=headers)
+    beldan = response.json()
+    return render_template('playerinf.html', data=beldan)
+
+
+
+
+@app.route('/transferlist')
+def transfer():
+    url = "https://sportapi7.p.rapidapi.com/api/v1/transfer"
+
+    headers = {
+        "X-RapidAPI-Key": "eea92a9621mshbb5f39383ef16f2p147c78jsn3b2a2ac99577",
+        "X-RapidAPI-Host": "sportapi7.p.rapidapi.com"
+}
+
+    response = requests.get(url, headers=headers)
+    naufal = response.json()
+    return render_template('transfer.html', data=naufal)
+
+@app.route('/event')
+def event():
+    url = "https://f1-motorsport-data.p.rapidapi.com/news"
+
+    headers = {
+	"X-RapidAPI-Key": "2a0c2b49bamshadfd80b51fc1deap12ea8fjsn8dedc40a0b81",
+	"X-RapidAPI-Host": "f1-motorsport-data.p.rapidapi.com"
+}
+
+    response = requests.get(url, headers=headers)       
+    difki = response.json()
+    return render_template('event.html', data=difki)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host= '0.0.0.0')
